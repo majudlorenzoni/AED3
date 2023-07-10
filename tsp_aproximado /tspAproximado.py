84b7d35bfcd5ff4019a419045be16684c1b3df03
Maria Júlia <105013681+majudlorenzoni@users.noreply.github.com>
2023-07-10 20:59:01 -0300

    Create tspAproximado.py

---
+++ tsp_aproximado /tspAproximado.py
@@ -0,0 +1,8 @@
+#importando bibliotecas
+
+from time import time
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import minimum_spanning_tree
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
